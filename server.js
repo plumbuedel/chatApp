5628db355dc3eee48366d9d064cc9ce2f3b613cb
cj <chrisjunge@gmx.de>
2020-03-05 22:37:42 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,16 @@
+const io = require('socket.io')(3000);
+const users = {};
+
+io.on('connection', socket => {
+    socket.on('send-chat-message', message => {
+        socket.broadcast.emit('chat-message',{ message: message, name: users[socket.id]} );
+    });
+    socket.on('new-user', name => {
+        users[socket.id] = name;
+        socket.broadcast.emit('user-connected-message', name);
+    });
+    socket.on('disconnect', () => {
+        socket.broadcast.emit('user-disconnected-message', users[socket.id]);
+        delete users[socket.id];
+    })
+});
